a2c9fa873ba359bd9828bde705c3f2c07355ad75
Alvin Tan <alvin.tan1912@gmail.com>
2024-01-03 00:17:33 +0800

    done layout for login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,7 @@
-import { H2, Muted } from "@/components/ui/typography";
 import { Metadata } from "next";
+import { H2, Muted } from "@/components/ui/typography";
 import Link from "next/link";
+import LoginForm from "../ui/login-form";
 
 export const metadata: Metadata = {
   title: "Login",
@@ -9,13 +10,14 @@ export const metadata: Metadata = {
 export default function LoginPage() {
   return (
     <main className="flex flex-col justify-center items-center h-screen">
-      <H2>Welcome to Echoize!</H2>
-      <Muted>
+      <H2 className="mb-2">Welcome to Echoize!</H2>
+      <Muted className="mb-8">
         Don&#39;t have an account?{" "}
         <Link className="text-blue-400 font-semibold" href="/">
-          Create one
+          Create one!
         </Link>
       </Muted>
+      <LoginForm />
     </main>
   );
 }
---
+++ app/ui/login-form.tsx
@@ -0,0 +1,40 @@
+"use-client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+export default function LoginForm() {
+  return (
+    <form>
+      <Card className="w-[350px]">
+        <CardHeader />
+        <CardContent>
+          <div className="grid w-full items-center gap-4">
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="username">Username</Label>
+              <Input id="username" placeholder="Enter your username" />
+            </div>
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="password">Password</Label>
+              <Input
+                id="password"
+                placeholder="Enter your password"
+                type="password"
+              />
+            </div>
+          </div>
+        </CardContent>
+        <CardFooter className="flex justify-end">
+          <Button>Login</Button>
+        </CardFooter>
+      </Card>
+    </form>
+  );
+}
