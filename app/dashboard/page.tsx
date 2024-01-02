e9215870ac763342d409ef5da0d91e6218b1453c
Alvin Tan <alvin.tan1912@gmail.com>
2024-01-03 00:25:07 +0800

    added dashboard route

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Dashboard",
+};
+
+export default function DashboardPage() {
+  return (
+    <main>
+      <h1>Dashboard</h1>
+    </main>
+  );
+}
