32c2cd53b4f4253cb8eb477601279def60a41644
Alvin Tan <alvin.tan1912@gmail.com>
2024-01-02 23:14:28 +0800

    added basic login page

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,10 +2,15 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+import { cn } from "@/lib/utils";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Echoize",
+  title: {
+    template: "%s | Echoize",
+    default: "Echoize",
+  },
   description: "Product feedback made simple.",
 };
 
@@ -16,7 +21,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={cn(`${inter.className} antialiased`)}>{children}</body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,12 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function Home() {
-  return <main></main>;
+  return (
+    <main className="flex justify-center items-center h-screen">
+      <Button>
+        <Link href="/login">Login</Link>
+      </Button>
+    </main>
+  );
 }
